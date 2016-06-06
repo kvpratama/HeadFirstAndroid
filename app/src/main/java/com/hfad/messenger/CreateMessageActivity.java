9452544cb6142113d1e4e49ba9bae75a14f92475
kvpratama <kvpratama@gmail.com>
2016-06-06 21:30:57 +0800

    Chapter 3 : Intent

---
+++ app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
@@ -0,0 +1,43 @@
+package com.hfad.messenger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.hfad.headfirstandroid.R;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+
+    }
+
+    //Call onSendMessage() when button @+id/send is clicked
+    public void onSendMessage(View view){
+        EditText editText = (EditText) findViewById(R.id.message);
+
+//        Explicit Intent
+//        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+//        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, editText.getText().toString());
+
+        //Implicit Intent
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, editText.getText().toString());
+
+        String chooserTitle = getString(R.string.chooser);
+        Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+
+        startActivity(chosenIntent);
+    }
+
+}
---
+++ app/src/main/java/com/hfad/messenger/ReceiveMessageActivity.java
@@ -0,0 +1,28 @@
+package com.hfad.messenger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.hfad.headfirstandroid.R;
+
+public class ReceiveMessageActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+
+        Intent intent = getIntent();
+        TextView textView = (TextView) findViewById(R.id.message);
+        textView.setText(intent.getStringExtra(EXTRA_MESSAGE));
+    }
+
+}
